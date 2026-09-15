f43a0a17af61fa8b75e642e17f73b681200373b1
lpmalosso <lpmalosso@gmail.com>
2026-09-15 15:21:51 -0300

    Comentários adicionais em "todo-pro-professor/TodoForm.jsx"

--- todo-pro-professor/src/components/TodoForm.jsx
+++ todo-pro-professor/src/components/TodoForm.jsx
@@ -1,17 +1,24 @@
+// Componente que vai gerenciar somente o formulário de envio
 // Formulário para adicionar tarefas
 
 import { useState } from "react"
 
 const TodoForm = ({addTask}) => {
-    const [task, setTask] = useState("");
+    // Estado local para armazenamento temporário do texto digitado no campo de input
+    const [task, setTask] = useState(""); // useState -> Armazenamento temporário
+    // No react usamos o const para evitar mudanças acidentais, sempre reatribuindo o valor ao usá-lo
 
+    // Cria um mainpulador de evento ao enviar o formulário -> handle
     const handleSubmit = (e) => {
         e.preventDefault(); // Não recarrega a página ao enviar o formulário
+        
+        // Validação do campo de entrada para verificar se o texto não é vazio
         if (task.trim() !== "") {
-            addTask(task.trim());
-            setTask("");
+            addTask(task.trim()); // Adiciona a tarefa na função do prop() -> poder enviar dados para um componente "pai"
+            setTask(""); // Reseta o campo de entrada para o valor inicial
         }
     }
+    // Virtual DOM
     return (
         <form onSubmit={handleSubmit}>
             <input type="text" name="" id="" value={task} onChange={(e) => setTask(e.target.value)}/>
